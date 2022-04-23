932c2fbfcfc01673cfc5fc596a9240f37ceba957
yayat9744 <yayat9744@gmail.com>
2022-04-23 22:26:22 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("/", "html");
+app.use(express.static(path.join(__dirname, "/")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8080, () => {
+    console.log("listening on http://localhost:8080");
+})
